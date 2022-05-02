5107d8cdf99c508929682b25b2a1201160a2cf29
Daniel Flassak <daniel.flassak@dm.de>
2022-05-02 20:53:08 +0200

    remove irrelevant TODOs

--- build.sh
+++ build.sh
@@ -3,8 +3,6 @@
 mvn clean package
 
 # TODO: see if there is a mvn plugin for jpackage
-# TODO: package only with the required modules
 # TODO: set the dock name and icon somehow
-# TODO: set the about dialog content
 cd target
 jpackage --name "LTC Rallye Tag Manager" --input . --main-jar tagmanager.jar
