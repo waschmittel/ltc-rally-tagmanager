2823c874ba8180c630dd46951b90d3afb49cb80e
Daniel Flassak <daniel.flassak@dm.de>
2023-03-11 20:46:21 +0100

    upddate dependencies

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# set -x # set this for debugging to print all called commands
+set -e # fail non non-zero exit status
+set -u # fail on unset variable
+set -o pipefail # fail if a pipe fails
+
 mvn clean package
 
 # TODO: see if there is a mvn plugin for jpackage
