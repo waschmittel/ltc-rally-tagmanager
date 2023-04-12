d0ed8c31b35e21e3d7b5cc7b3376560132cfdefa
Daniel Flassak <daniel.flassak@dm.de>
2023-04-12 13:25:28 +0200

    use import

--- src/main/java/de/flubba/tagmanager/TagManager.java
+++ src/main/java/de/flubba/tagmanager/TagManager.java
@@ -5,6 +5,7 @@ import de.flubba.tagmanager.ui.UI;
 import de.flubba.tagmanager.util.OsType;
 
 import javax.swing.BorderFactory;
+import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -24,6 +25,6 @@ public final class TagManager {
 
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(UI::createAndShow);
+        SwingUtilities.invokeLater(UI::createAndShow);
     }
 }
