46b64d39fd45442261c928538021558b065ffc25
Daniel Flassak <daniel.flassak@dm.de>
2024-03-15 22:21:38 +0100

    add meta information to build

--- build.bat
+++ build.bat
@@ -4,4 +4,12 @@ call mvn clean package
 cd target
 md package
 move tagmanager.jar package
-jpackage --name "LTC Rallye Tagmanager" --input package --main-jar tagmanager.jar --type app-image
+jpackage \
+    --name "LTC Rallye Tag Manager" \
+    --input . --icon ../res/icon/app-icon.icns \
+    --main-jar tagmanager.jar \
+    --about-url "https://github.com/waschmittel/ltc-rally-tagmanager" \
+    --copyright "(C) 2024 Daniel Flassak" \
+    --description "Client for LTC Rallye Software for assigning NFC tags and counting laps." \
+    --vendor "Flubba" \
+    --app-version "3.141592"
--- build.sh
+++ build.sh
@@ -8,4 +8,12 @@ set -o pipefail # fail if a pipe fails
 mvn clean package
 
 cd target
-jpackage --name "LTC Rallye Tag Manager" --input . --icon ../res/icon/app-icon.icns --main-jar tagmanager.jar
+jpackage \
+    --name "LTC Rallye Tag Manager" \
+    --input . --icon ../res/icon/app-icon.icns \
+    --main-jar tagmanager.jar \
+    --about-url 'https://github.com/waschmittel/ltc-rally-tagmanager' \
+    --copyright '(C) 2024 Daniel Flassak' \
+    --description 'Client for LTC Rallye Software for assigning NFC tags and counting laps.' \
+    --vendor 'Flubba' \
+    --app-version '3.141592'
