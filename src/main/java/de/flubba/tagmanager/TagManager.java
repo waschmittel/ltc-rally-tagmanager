8811765f721d3da2bdd44a8acf8880925a9e6c1a
Daniel Flassak <daniel.flassak@dm.de>
2024-05-14 22:47:49 +0200

    remove unnecessary border config

--- src/main/java/de/flubba/tagmanager/TagManager.java
+++ src/main/java/de/flubba/tagmanager/TagManager.java
@@ -5,7 +5,6 @@ import de.flubba.tagmanager.discovery.MDNSListener;
 import de.flubba.tagmanager.ui.UI;
 import de.flubba.tagmanager.util.OsType;
 
-import javax.swing.BorderFactory;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -22,7 +21,6 @@ public final class TagManager {
             System.setProperty("apple.awt.application.name", "LTC Rallye Tag Manager");
         }
         UIManager.setLookAndFeel(new FlatIntelliJLaf());
-        UIManager.getDefaults().put("SplitPane.border", BorderFactory.createEmptyBorder());
 
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
