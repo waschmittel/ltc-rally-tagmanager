a8b0fb2ca227cc7635022243e6a12354bf348ff6
Daniel Flassak <daniel.flassak@dm.de>
2024-05-14 22:45:27 +0200

    fix Windows build

--- build.bat
+++ build.bat
@@ -4,12 +4,16 @@ call mvn clean package
 cd target
 md package
 move tagmanager.jar package
-jpackage \
-    --name "LTC Rallye Tag Manager" \
-    --input . --icon ../res/icon/app-icon.icns \
-    --main-jar tagmanager.jar \
-    --about-url "https://github.com/waschmittel/ltc-rally-tagmanager" \
-    --copyright "(C) 2024 Daniel Flassak" \
-    --description "Client for LTC Rallye Software for assigning NFC tags and counting laps." \
-    --vendor "Flubba" \
-    --app-version "3.141592"
+cd package
+jpackage ^
+    --name "LTC Rallye Tag Manager" ^
+    --input . --icon ../../res/icon/app-icon.png ^
+    --main-jar tagmanager.jar ^
+    --about-url "https://github.com/waschmittel/ltc-rally-tagmanager" ^
+    --copyright "(C) 2024 Daniel Flassak" ^
+    --description "Client for LTC Rallye Software for assigning NFC tags and counting laps." ^
+    --win-menu ^
+    --vendor "Flubba" ^
+    --win-per-user-install ^
+    --type "exe" ^
+    --app-version "3.14"
