15b506237308344dc46cc17bd1769b9e123d0233
Daniel Flassak <daniel.flassak@dm.de>
2026-03-18 17:57:46 +0100

    hopefully fix mDNS listener

--- src/main/java/de/flubba/tagmanager/discovery/MDNSListener.java
+++ src/main/java/de/flubba/tagmanager/discovery/MDNSListener.java
@@ -7,7 +7,12 @@ import javax.jmdns.ServiceEvent;
 import javax.jmdns.ServiceListener;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Enumeration;
 
 @Slf4j
 public final class MDNSListener {
@@ -15,12 +20,39 @@ public final class MDNSListener {
 
     public static void listen() {
         try {
-            JmDNS jmDNS = JmDNS.create();
-            jmDNS.addServiceListener("_ltc-rallye._tcp.local.", new EventPublishingListener());
+            var jmDNS = JmDNS.create(getIPv4Address());
+            jmDNS.addServiceListener("_ltc-rallye._tcp.flubba.", new EventPublishingListener());
         } catch (IOException e) {
             throw new UncheckedIOException("There is a problem with mDNS.", e);
         }
     }
+
+    private static InetAddress getIPv4Address() throws IOException {
+        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+        var ipv4s = new ArrayList<Inet4Address>();
+        while (interfaces.hasMoreElements()) {
+            NetworkInterface networkInterface = interfaces.nextElement();
+            if (networkInterface.isLoopback()
+                    || !networkInterface.isUp()
+                    || networkInterface.isVirtual()
+                    || networkInterface.getName().startsWith("bridge") // on macOS bridge is virtual, but isVirtual() returns false for some readon
+            ) {
+                continue;
+            }
+            Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
+            while (addresses.hasMoreElements()) {
+                InetAddress address = addresses.nextElement();
+                if (address instanceof Inet4Address) {
+                    ipv4s.add((Inet4Address) address);
+                }
+            }
+        }
+
+        ipv4s.forEach(v4 -> log.info("Found IPv4 address for mDNS: {}", v4.getHostAddress()));
+
+        return ipv4s.stream().findFirst().orElseThrow(() -> new IOException("No IPv4 address found"));
+    }
+
 
     private static final class EventPublishingListener implements ServiceListener {
         @Override
